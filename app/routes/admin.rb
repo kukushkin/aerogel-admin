2a317324156484d7e537765837732b814aef7cb5
Alex Kukushkin <alex@kukushk.in>
2014-02-27 21:41:44 +0200

    Initial page layout, assets for aerogel-admin

---
+++ app/routes/admin.rb
@@ -0,0 +1,31 @@
+namespace "/admin" do
+
+  before do
+    layout "admin"
+
+    on_access_denied do |path|
+      redirect "/user/login?on_success=#{path}" unless current_user?
+      redirect "/admin/" if can? "/admin/"
+    end
+
+  end
+
+  get "/" do
+    flash[:notice] = "Welcome, admin!"
+    pass
+  end
+
+  get "/lorem" do
+    view "lorem"
+  end
+
+  #
+  # Common User controller routes
+  #
+  route :get, :post, ['/', '/:action'] do
+    action = params[:action] || 'index'
+    view "admin/#{action}"
+  end
+
+
+end
